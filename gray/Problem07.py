bcd690d9aac7e4f594a271f0d9a834806d00193c
Graydon Beeman <gbeeman@thornburg.com>
2017-10-31 11:32:28 -0600

    problem 7

---
+++ gray/Problem07.py
@@ -0,0 +1,12 @@
+n = 5
+numbers = [2,3]
+while len(numbers) < 10001:
+    for number in numbers:
+        if n%number == 0:
+            tempResult = True
+            break
+    if tempResult == False:
+        numbers.append(n)
+    tempResult = False
+    n += 2
+print (numbers[10000])
