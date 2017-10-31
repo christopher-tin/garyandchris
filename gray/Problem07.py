970b5893f59f2026b80b7056ba0b58703f3d8dd8
Graydon Beeman <gbeeman@thornburg.com>
2017-10-31 13:49:17 -0600

    Problem 7 & 8

--- gray/Problem07.py
+++ gray/Problem07.py
@@ -1,3 +1,6 @@
+tempResult = False
+finalResult = False
+
 n = 5
 numbers = [2,3]
 while len(numbers) < 10001:
@@ -10,3 +13,5 @@ while len(numbers) < 10001:
     tempResult = False
     n += 2
 print (numbers[10000])
+
+            
